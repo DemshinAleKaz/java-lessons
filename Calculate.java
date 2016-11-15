01277ce7c65705c293e2ce2d625079c5defd5a28
Aleksei <demsinaleksej@gmail.com>
2016-11-15 23:30:05 +0300

    new Calculate

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,15 @@
 public class Calculate{
-	public static void main(String[] args){
+	public static void main(String[] arg){
         System.out.println("Calculate...");
+	int a = Integer.valueOf(arg[0]);
+	int b = Integer.valueOf(arg[1]);
+        int sum = a + b;
+	int summ = a - b;
+	int sumn = a / b;
+	int sumt = a * b;
+	System.out.println("Sum " + sum);
+	System.out.println("Sum " + summ);
+	System.out.println("Sum " + sumn);
+	System.out.println("Sum " + sumt);
 } 
 }
